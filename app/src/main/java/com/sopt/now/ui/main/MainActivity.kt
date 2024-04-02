b08c146f6e0576989e487a05ed0855afea27ce60
junseo511 <prime54@g.skku.edu>
2024-04-02 15:47:00 +0900

    feat: 메인 화면 UI 제작

--- app/src/main/java/com/sopt/now/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/now/ui/main/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.sopt.now.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.R
+import com.sopt.now.ui.signUp.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    companion object {
+        private const val USER_NAME = "USER_NAME"
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, SignUpActivity::class.java).apply {
+                putExtra(USER_NAME, "USER_NAME")
+            }
+        }
     }
 }
