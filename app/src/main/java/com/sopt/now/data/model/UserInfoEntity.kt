e29b5c682910139088551250f79769a17ce96b3e
junseo511 <prime54@g.skku.edu>
2024-04-16 01:43:15 +0900

    refactor: 불필요한 어노테이션 제거

--- app/src/main/java/com/sopt/now/data/model/UserInfoEntity.kt
+++ app/src/main/java/com/sopt/now/data/model/UserInfoEntity.kt
@@ -1,14 +1,13 @@
 package com.sopt.now.data.model
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "user_table")
 data class UserInfoEntity(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    @ColumnInfo val username: String,
-    @ColumnInfo val password: String,
-    @ColumnInfo val nickname: String,
-    @ColumnInfo val drinkCapacity: Float,
+    val username: String,
+    val password: String,
+    val nickname: String,
+    val drinkCapacity: Float,
 )
