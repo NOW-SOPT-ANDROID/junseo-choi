c7eebc164e36af8a52eaf0f59acf34fe13249c30
junseo511 <prime54@g.skku.edu>
2024-04-16 20:00:29 +0900

    refactor: 불필요한 변수 제거

--- app/src/main/java/com/sopt/now/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sopt/now/ui/main/MainViewModel.kt
@@ -7,16 +7,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sopt.now.data.model.UserInfoEntity
 import com.sopt.now.data.repository.UserRepository
-import com.sopt.now.ui.main.home.HomeAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel(private val userRepository: UserRepository) : ViewModel() {
     private val _userInfo = MutableLiveData<UserInfoEntity>(UserInfoEntity.defaultUserInfo)
     val userInfo: LiveData<UserInfoEntity> = _userInfo
-
-    private val _homeAdapter = MutableLiveData<HomeAdapter>()
-    val homeAdapter: LiveData<HomeAdapter> = _homeAdapter
 
     fun getUserInfo(username: String) {
         viewModelScope.launch(Dispatchers.IO) {
