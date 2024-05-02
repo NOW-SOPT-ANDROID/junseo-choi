ff7030fc7f6bfb29443a9a0b9cb7084f7259fb64
junseo511 <prime54@g.skku.edu>
2024-05-02 09:30:13 +0900

    feat: 네트워크 서비스 추가

---
+++ app/src/main/java/com/sopt/now/compose/data/remote/service/FriendService.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.compose.data.remote.service
+
+import com.sopt.now.data.remote.response.GetFriendsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface FriendService {
+    @GET("users")
+    suspend fun getFriends(
+        @Query("page") page: Int,
+    ): Response<GetFriendsResponse>
+}
